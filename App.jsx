f665190837392c9b473959685ad3a5e17a6d08f4
EvgenePirat <brexynckenko@sunpoint.com.ua>
2024-09-23 19:08:28 +0300

    #Fix mistake with app

--- App.jsx
+++ App.jsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import {
-  SafeAreaView
-} from 'react-native';
 import Splash from './src/screens/auth/Splash';
 import Signup from './src/screens/auth/Signup';
 import Signin from './src/screens/auth/Signin';
